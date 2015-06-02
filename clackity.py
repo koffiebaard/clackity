6b24798710baa91e5c9e0bf27cd9677fdfe1c1ff
Wisc <wisc.whut@gmail.com>
2015-06-02 14:41:06 +0200

    arr

--- clackity.py
+++ clackity.py
@@ -2,10 +2,43 @@
 
 import urllib2
 import sys
+import json
+import MySQLdb
 
+# Settings
+fp = open('../clackity.json')
+settings = json.load(fp)
+print settings
+
+# Request
 class HeadRequest(urllib2.Request):
     def get_method(self):
         return "HEAD"
+
+# DB connection
+db = MySQLdb.connect(host=settings["db_host"],
+                         user=settings["db_username"],
+                         passwd=settings["db_password"],
+                         db="clackity")
+
+def sendsClacks(url):
+  cur = self.db.cursor()
+  cur.execute("select * from `clacks_senders` where `url` = '%s'" % url)
+  arr = cur.fetchone()
+  cur.close()
+  return arr
+
+def receivedClacksFrom(self, url):
+
+  if self.db is None:
+    self.connectDB()
+
+  cur = self.db.cursor()
+  cur.execute("""
+	insert into `clacks_senders` (`url`) values ('%s')
+  """, (url,))
+  self.db.commit()
+  cur.close()
 
 
 if len(sys.argv) > 1:
@@ -15,4 +48,5 @@ if len(sys.argv) > 1:
 
     for header in response.headers:
         if header == "x-clacks-overhead":
-            print "yarr"
+            receivedClacksFrom(url)
+            print "yarr!"
